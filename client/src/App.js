60617b67588b7c33047005f69bcbc29d91f1480a
Kush-316 <kushagrakapoor619@gmail.com>
2025-04-10 21:15:53 +0530

    Clean re-add: client folder as a regular directory

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <img src="" alt="" />
+        <h2>New Matches announced for WrestleMania 41</h2>
+        <p>Drew McIntyre Vs Damian Priet announced for WrestleMania 41.</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
