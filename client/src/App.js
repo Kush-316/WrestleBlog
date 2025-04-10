51bd80788849119669e917f5afde9aeeb0dce254
Kush-316 <kushagrakapoor619@gmail.com>
2025-04-11 00:11:04 +0530

    update1

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
-
+import Post from './post';
+import Header from './header';
+import {Route, Routes} from "react-router-dom";
+import Layout from './layout';
+import IndexPage from './pages/indexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <img src="" alt="" />
-        <h2>New Matches announced for WrestleMania 41</h2>
-        <p>Drew McIntyre Vs Damian Priet announced for WrestleMania 41.</p>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div class="image">
+        <img src="https://www.wwe.com/f/styles/gallery_img_l/public/2024/09/wm41_tix_fein_final_rev_1--49831a53032fa6e4a3e6051155198236.jpg" alt="" />
+        </div>
+        <div className="text">
+        <h2>New Matches announced for WrestleMania 41</h2>
+        <p class="info">
+          <a class="author">Kush</a>
+          <time>2025-04-10 22:59</time>
+        </p>
+        <p className="summary">Drew McIntyre Vs Damian Priest announced for WrestleMania 41.</p>
+        </div>
+      </div>
+    );
+}
